300a122597239b6afbe43a62cad2e9b6d876188c
chenming0416 <cjmcjh1118>
2020-03-27 13:22:06 +0800

    add Headless Browser,增加无头浏览器

--- src/test/java/testcase/testCaseTest2.java
+++ src/test/java/testcase/testCaseTest2.java
@@ -1,7 +1,9 @@
 package testcase;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,7 +15,23 @@ public class testCaseTest2 {
     @Test
     public void test2(){
         System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        //创建无Chrome无头参数
+        ChromeOptions chromeOptions=new ChromeOptions();
+        chromeOptions.addArguments("-headless");
+
+        //创建Drive实例
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        //WebDriver driver = new ChromeDriver();
+        driver.get("https://www.baidu.com");
+        driver.findElement(By.id("kw")).sendKeys("hello");
+        driver.findElement(By.id("su")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String aa = driver.findElement(By.xpath("//*[@id='container']/div[2]/div/div[2]/span")).getText();
+        System.out.println(aa);
         System.out.println("testCase222 ");
     }
     @BeforeMethod
